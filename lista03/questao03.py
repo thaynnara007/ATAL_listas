b4dc0490e4fbbc2481fef818cf749e3fd078d914
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-11-25 16:20:13 -0300

    adicionando questao03

---
+++ lista03/questao03.py
@@ -0,0 +1,19 @@
+def numBinomial(n, k):
+
+    global dp
+
+    for i in xrange(0, n):
+
+        for j in xrange(k , 0, -1):
+
+            dp[j] = dp[j] + dp[j - 1] #subestrutura otima
+
+
+n = int(raw_input("n:"))
+k = int(raw_input("k:"))
+dp = [0 for i in xrange(k + 1)]
+dp[0] = 1 #caso base
+
+numBinomial(n,k)
+print dp
+print "c(%d, %d) = %d" %(n,k,dp[-1])
