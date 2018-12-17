054b643fb194a8ababfa491ca70a3fb9979c58ae
thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2018-12-17 14:55:38 -0300

    lista04, questao02

---
+++ lista04/questa02.py
@@ -0,0 +1,45 @@
+#dfs
+BRANCO = 1
+CINZA = 2
+PRETO = 3
+
+def myDFS(no):
+
+    global listaAdjacencia
+    global cor
+
+    cor[no] = CINZA
+    temCiclo = False
+    print cor
+    print no
+    for adjacente in listaAdjacencia[no]:
+
+        if cor[adjacente] == BRANCO: temCiclo = temCiclo or myDFS(adjacente)
+        elif cor[adjacente] == PRETO: temCiclo = True
+    
+    cor[no] = PRETO    
+    return temCiclo
+
+#lendo o grafo
+qntdNos, qntdArestas = map(int, raw_input("quantidade de vertices, quantidade de arestas:").split())
+cor = {}
+listaAdjacencia = {}
+print "no1, no2:"
+for aresta in xrange(qntdArestas):
+
+    no1, no2 = raw_input().split()
+
+    if no1 not in listaAdjacencia:
+        listaAdjacencia[no1] = [no2]
+        cor[no1] = BRANCO
+    else: listaAdjacencia[no1].append(no2)
+
+    if no2 not in listaAdjacencia:
+        listaAdjacencia[no2] = [no1]
+        cor[no2] = BRANCO
+    
+    else: listaAdjacencia[no2].append(no1)
+print listaAdjacencia
+
+root = raw_input("escolha o vertice inicial:")
+print myDFS(root)
